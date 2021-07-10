0d6a873929a141be23151dc14b1593c4d2bfb140
raminduw200 <raminduw200@gmail.com>
2021-07-11 00:36:57 +0530

    Add comments: Explaining the functions variables

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
+/*
+ * Name     :   Ramindu Walgama
+ * TempNO   :   201177
+ * Email    :   raminduw200@gmail.com
+ */
+
 #include "stack.c"
 #include <string.h>
 
+// get the char value of a given integer. 1 -> temp[1]
 char temp[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-int temp_no = 1;
+int temp_no = 1;  // keep the track of temp variable number
+// var1, var2: temporary variables to keep the pop elements of the stack
 char var1;
 char var2;
 
+/*
+ * @function: check a char is an integer (Ex. '2')
+ * @return: true: if char is an integer. else false
+ */
 int isTemp(char a){
     for (int i = 1; i < 10; ++i) {
         if(temp[i] == a)     return 1;
@@ -13,10 +25,23 @@ int isTemp(char a){
     return 0;
 }
 
+/*
+ * @function: Do the given instruction(+,-,*, and /) for the
+ * next two variables in the stack.
+ * @param: instruction in below format.
+ *         LD A which places the operand A into the register.
+ *         ST A which places the contents of the register into the variable A.
+ *         AD A which adds the contents of variable A to the register.
+ *         SB A which subtracts the contents of the variable A from the register.
+ *         ML A which multiplies the contents of the register by the variable A.
+ *         DV A which divides the contents of the register by the variable A.
+ * @return: void | Print the process
+ */
 void process(char *instruction){
     var1 = pop();
     var2 = pop();
 
+    // Decide the print place of TEMPn variable and print.
     if (isTemp(var1) && isTemp(var2)) {
         printf("LD \tTEMP%c \n%s \tTEMP%c\nST \tTEMP%d\n", var2, instruction, var1, temp_no);
     }  else if (isTemp(var2)){
@@ -27,14 +52,19 @@ void process(char *instruction){
         printf("LD \t%c \n%s \t%c\nST \tTEMP%d\n", var2, instruction, var1, temp_no);
     }
 
-    push(temp[temp_no++]);
+    // Reset if the temp_no variable if exceed 9
+    if (temp_no < 10) {
+        push(temp[temp_no++]);
+    } else {
+        temp_no = 0;
+    }
 }
 
 int main() {
     char postfix[50];
 
     printf("Enter the postfix expression : ");
-    scanf("%s", postfix);
+    scanf("%s", postfix);  //ABC*+DE-/
 
     for(int i = 0; i < strlen(postfix); ++i){
         if(postfix[i] == '+'){
@@ -46,6 +76,7 @@ int main() {
         } else if(postfix[i] == '/'){
             process("DV");
         } else {
+            // pushing each character to the stack if its not not an operation
             push(postfix[i]);
         }
     }
--- stack.c
+++ stack.c
@@ -1,22 +1,47 @@
+/*
+ * Name     :   Ramindu Walgama
+ * TempNO   :   201177
+ * Email    :   raminduw200@gmail.com
+ */
+
 #include <stdio.h>
 
 int top = -1;
 const int size = 50;
 char stack[50];
 
+/*
+ * NOTE
+ * that all the methods are not being used in the implementation of
+ * single register process but used for debugging and testing purposes.
+ */
+
+/*
+ * Checks the stack is full
+ * @return: boolean if stack is full
+ */
 int isFull(){
     return top == size+1;
 }
 
+/*
+ * Checks the stack is empty
+ * @return: boolean if stack is empty
+ */
 int isEmpty(){
     return top == -1;
 }
 
+// @return: int | size of the stack
 int getSize(){
     return size;
 }
 
-int peek(){
+/*
+ * Get the top element of the stack without removing it.
+ * @return: char | top element of the stack
+ */
+char peek(){
     if (!isEmpty())
         return stack[top];
     else
@@ -24,14 +49,21 @@ int peek(){
     return 0;
 }
 
-char push(char value_){
+/*
+ * Add a character to the stack
+ * @return: void
+ */
+void push(char value_){
     if (!isFull())
         stack[++top] = value_;
     else
         printf("[ERROR-PUSH] Stack Overflow: Stack is FULL!\n");
-    return 0;
 }
 
+/*
+ * Remove the top element of the stack.
+ * @return: char | top element of the stack
+ */
 char pop(){
     if (!isEmpty())
         return stack[top--];
@@ -40,6 +72,9 @@ char pop(){
     return 0;
 }
 
+/*
+ * Display all the elements in the stack
+ */
 void display(){
     printf("[STACK] Printing the stack... \n");
     for (int i = top; i != -1; --i)
